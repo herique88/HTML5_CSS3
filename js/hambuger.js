319ccf7390ca484ecc75b7494ab4e606869bf67e
joao_hs <glasnote@gmail.com>
2024-05-10 15:00:30 -0300

    correção de bug

---
+++ js/hambuger.js
@@ -0,0 +1,14 @@
+
+window.onload = function() {
+    let btn = document.querySelector(".menu");
+
+    btn.addEventListener("click", function(e){
+      
+        if(e.target.href != null) {
+            document.getElementById('check').checked = false;
+            btn.display = false;
+        }
+      
+    });
+};
+
